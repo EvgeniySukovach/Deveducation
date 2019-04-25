87267a16809c87ff910b456f7ac2696a66755b72
EvgeniySukovach <esdnepr@gmail.com>
2019-04-25 12:33:14 +0300

    qwe

---
+++ C#/Citizen/Citizen/Class1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit;
+using NUnit.Framework;
+
+namespace Citizen
+{
+    
+    class Class1
+
+
+    {
+        [Test]
+        public void myTest() {
+            var test = 2 + 2;
+            Assert.AreEqual(4, test);
+
+        }
+    }
+}
---
+++ C#/Citizen/Citizen/Form1.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Citizen
+{
+    public partial class Form1 : Form
+    {
+        Double result = 0;
+        String operation = "";
+        bool action = false;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Click(object sender, EventArgs e){}
+
+        private void button_Click(object sender, EventArgs e)
+        {
+            if ((textBox.Text == "0") || action)
+                textBox.Clear();
+            action = false;
+
+            Button button = (Button)sender;
+
+            if (button.Text == ",") {
+                if (!textBox.Text.Contains(","))
+                    textBox.Text = textBox.Text + button.Text;
+                
+            }
+            else textBox.Text = textBox.Text + button.Text;
+
+            //textBox.Text = textBox.Text + button.Text;
+        }
+
+        private void opetator_Click(object sender, EventArgs e)
+        {
+            Button button = (Button)sender;
+            operation = button.Text;
+            result = Double.Parse(textBox.Text);
+            action = true;
+        }
+
+        private void button18_Click(object sender, EventArgs e)
+        {
+            textBox.Text = "0";
+        }
+
+        private void button17_Click(object sender, EventArgs e)
+        {
+            textBox.Text = "0";
+            result = 0;
+        }
+
+        private void calculate(object sender, EventArgs e)
+        {
+            switch (operation) {
+                case "+":
+                    textBox.Text = (result + Double.Parse(textBox.Text)).ToString();
+                    break;
+                case "-":
+                    textBox.Text = (result - Double.Parse(textBox.Text)).ToString();
+                    break;
+                case "*":
+                    textBox.Text = (result * Double.Parse(textBox.Text)).ToString();
+                    break;
+                case "/":
+                    textBox.Text = (result / Double.Parse(textBox.Text)).ToString();
+                    break;
+                default:
+                    break;
+
+            }
+        }
+    }
+}
